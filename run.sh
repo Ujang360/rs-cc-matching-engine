6e8cf773078eeaa31c05a3da018a7928fdfc7236
Aditya Kresna <aditya.kresna@outlook.co.id>
2019-12-08 16:43:00 +0700

    Change package name

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 clear && \
-    RUST_BACKTRACE=1 cargo test && \
+    RUST_BACKTRACE=full cargo test && \
     cargo build --release && \
-    cp target/release/rs-cc-matching-engine cc-matching-engine.out && \
+    cp target/release/cc-matching-engine cc-matching-engine.out && \
     strip cc-matching-engine.out && \
     clear && \
     ./cc-matching-engine.out
